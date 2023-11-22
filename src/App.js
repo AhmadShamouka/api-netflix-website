83a1f63541c1730d4a2796aaf66063dd6ede64c0
rachad <helal.rachad@gmail.com>
2023-11-22 17:55:41 +0200

    rachad files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
 import './App.css';
+import Footer from './Header-footer/rachad-footer-component/Footer';
+import "./Header-footer/rachad-footer-component/rachad-footer.css";
+import Header from './Header-footer/rachad-header-component/Header';
+import './Header-footer/rachad-header-component/rachad-header.css';
 
 function App() {
   return (
     <div className="App">
-      HELLO
+      <Header />
+      <Footer />
       
     </div>
   );
