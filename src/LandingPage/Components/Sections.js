5dfce5530fc51ef7fc67bb8b84fed16ab0803c62
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-11-22 16:19:04 +0200

    add sections component with section three

--- src/LandingPage/Components/Sections.js
+++ src/LandingPage/Components/Sections.js
@@ -93,8 +93,36 @@ const Sections = () => {
 
         {/* Section Three */}
         <div className="feature">
-          {/* Section Three Content */}
+      <div className="feature__details">
+        <h3 className="feature__title">Watch everywhere</h3>
+        <h5 className="feature__sub__title">
+          Stream unlimited movies and TV shows on your phone, 
+           tablet, laptop, and TV.
+        </h5>
+      </div>
+      <div className="feature__image__container feature__3__image__container">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/device-pile.png"
+          alt="Feature image"
+          className="feature__image feature__3__image"
+        />
+        <div
+          className="feature__backgroud__video__container feature__3__backgroud__video__container"
+        >
+          <video
+            autoPlay
+            loop
+            muted
+            className="feature__backgroud__video feature__3__backgroud__video"
+          >
+            <source
+              src="https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/video-devices.m4v"
+              type="video/mp4"
+            />
+          </video>
         </div>
+      </div>
+    </div>
 
         {/* Section Four */}
         <div className="feature">
