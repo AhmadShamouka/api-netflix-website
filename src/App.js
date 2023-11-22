1173f8b83e2502a7994e1c18582eeac92365c82f
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-11-22 16:21:43 +0200

    add FAQ section to App component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './LandingPage/Components/Header';
 import Hero from './LandingPage/Components/Hero';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sections from './LandingPage/Components/Sections';
+import FAQSection from './LandingPage/Components/FAQSection';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
               <Header/>
               <Hero/>
                <Sections/>
+               <FAQSection/>
             </React.Fragment>
           }/>
          
