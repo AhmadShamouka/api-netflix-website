98846dd547727ddd0fa65fd35937b99760b98370
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-11-22 16:19:37 +0200

    add sections component with section four

--- src/LandingPage/Components/Sections.js
+++ src/LandingPage/Components/Sections.js
@@ -126,8 +126,21 @@ const Sections = () => {
 
         {/* Section Four */}
         <div className="feature">
-          {/* Section Four Content */}
-        </div>
+      <div className="feature__details">
+        <h3 className="feature__title">Create profiles for Kids</h3>
+        <h5 className="feature__sub__title">
+        Send kids on adventures with their favorite characters in 
+        a space made just for them—free with your membership.
+        </h5>
+      </div>
+      <div className="feature__image__container">
+        <img
+          src="https://occ-0-4023-2164.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABVxdX2WnFSp49eXb1do0euaj-F8upNImjofE77XStKhf5kUHG94DPlTiGYqPeYNtiox-82NWEK0Ls3CnLe3WWClGdiJP.png?r=5cf"
+          alt="Feature image"
+          className="feature__image"
+        />
+      </div>
+    </div>
       </div>
     </HelmetProvider>
   );
