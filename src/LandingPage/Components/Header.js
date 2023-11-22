8c8abdc62bc974b8682a482856e9c8dfceaae23e
Ghadeer-Zahwe <ghadeerzahwe1@gmail.com>
2023-11-22 15:53:31 +0200

    add header

---
+++ src/LandingPage/Components/Header.js
@@ -0,0 +1,47 @@
+
+import React from 'react';
+import './Landing.css'
+
+const Header = () => {
+    return (
+  
+      <header>
+        <nav className="navbar">
+          <div className="navbar__brand">
+            <img
+              src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
+              alt="logo"
+              className="brand__logo"
+            />
+          </div>
+  
+          <div className="navbar__nav__items">
+            <div className="nav__item">
+              <div className='item_select'>
+                <i className="fas fa-globe"></i>
+                <select
+                  name="languages"
+                  id="languagesSelect"
+                  className="language__drop__down"
+                >
+                       <option value="arabic"  style={{ color: 'grey' }}>
+                    العربية
+                  </option>
+                  <option value="english" selected style={{ color: 'grey' }}>
+                    English
+                  </option>
+               
+                </select>
+            </div>
+         </div>
+  
+            <div className="nav__item">
+              <a className="signin__button">Sign in</a>
+            </div>
+          </div>
+        </nav>
+      </header>
+    );
+  };
+  
+  export default Header;
